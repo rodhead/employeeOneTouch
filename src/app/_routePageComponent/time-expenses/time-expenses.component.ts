07edceabed85121db60d47a447ef9ba1e3c285e1
rodHead <misbahrahman7300@yahoo.com>
2019-10-02 14:49:53 +0530

    Adding Dynamic Form for TimeSheet

--- src/app/_routePageComponent/time-expenses/time-expenses.component.ts
+++ src/app/_routePageComponent/time-expenses/time-expenses.component.ts
@@ -1,17 +1,51 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder, Validators, FormControl, FormArray, NgForm } from '@angular/forms'
 
 @Component({
   selector: 'app-time-expenses',
   templateUrl: './time-expenses.component.html',
   styleUrls: ['./time-expenses.component.css']
 })
-export class TimeExpensesComponent implements OnInit {
-  
+export class TimeExpensesComponent  {
+  addForm: FormGroup;
+  timeEntered=45;
+  rows: FormArray;
+  itemForm: FormGroup;
+  fileName:String="upload File";
   timeCategory:string[]=['GDJ-03-AL-0093','Leave', 'Travel','Client Site'];
-  constructor() { }
+  constructor(
+    private fb: FormBuilder
+  ) { 
+    this.addForm = this.fb.group({
+    });
+
+    this.rows = this.fb.array([]);
+
+  }
 
   ngOnInit() {
+    this.addForm.addControl('rows', this.rows);
+    this.rows.push(this.createItemFormGroup());
+  }
+
+  onAddRow() {
+    this.rows.push(this.createItemFormGroup());
+  }
+  onRemoveRow(rowIndex:number){
+    this.rows.removeAt(rowIndex);
+  }
+
+  createItemFormGroup(): FormGroup {
+    return this.fb.group({
+      timeCategory: null,
+      sun: null,
+      mon: null,
+      tue: null,
+      wed: null,
+      thu: null,
+      fri: null,
+      sat: null,
+    });
   }
 
   saveTimeSheet(){
